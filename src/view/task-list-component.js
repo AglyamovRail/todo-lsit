ef87d0c1f5e3c5036b592603a4d125f40592860d
AglyamovRail <rick552rick@gmail.com>
2024-11-23 15:21:33 +0300

    Update task-list-component.js, some fixes

--- src/view/task-list-component.js
+++ src/view/task-list-component.js
@@ -1,7 +1,6 @@
 import { AbstractComponent } from '../framework/view/abstract-component.js';
 
 function createTaskListComponentTemplate(listname, status) {
-<<<<<<< HEAD
   return (
     `<ul class="${status}-list">
       <h3 class="${status}">${listname}</h3>
@@ -16,16 +15,11 @@ function createTaskListComponentTemplate(listname, status) {
   return (
     `<ul class="${status}-list">
       <h3 class="${status}">${statusTranslation[status] || status}</h3>
->>>>>>> 12edd0b (added lab7)
       <ul class="task-list"></ul>
     </ul>`
   );
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 12edd0b (added lab7)
 export default class TaskListComponent extends AbstractComponent {
   constructor(listname, status, onTaskDrop) {
     super();
@@ -41,30 +35,23 @@ export default class TaskListComponent extends AbstractComponent {
 
   #setDropHandler() {
     const container = this.element.querySelector('.task-list');
-<<<<<<< HEAD
-    
     
     let targetIndex = null;
 
     
     container.addEventListener('dragover', (event) => {
       event.preventDefault();
-
-=======
   
     let targetIndex = null;
   
     container.addEventListener('dragover', (event) => {
       event.preventDefault(); 
   
->>>>>>> 12edd0b (added lab7)
       const targetElement = event.target.closest('.task');
       if (targetElement) {
         const taskList = Array.from(container.children);
         targetIndex = taskList.indexOf(targetElement);
-<<<<<<< HEAD
-        
-        
+
         taskList.forEach(task => task.classList.remove('drag-over'));
         targetElement.classList.add('drag-over');
       } else {
@@ -76,13 +63,9 @@ export default class TaskListComponent extends AbstractComponent {
     container.addEventListener('drop', (event) => {
       event.preventDefault();
       const taskId = event.dataTransfer.getData('text/plain');
-      
-      
+    
       container.querySelectorAll('.task').forEach(task => task.classList.remove('drag-over'));
-
-     
-=======
-  
+      
         taskList.forEach(task => task.classList.remove('drag-over'));
         targetElement.classList.add('drag-over');
       } else {
@@ -97,15 +80,10 @@ export default class TaskListComponent extends AbstractComponent {
   
       container.querySelectorAll('.task').forEach(task => task.classList.remove('drag-over'));
   
->>>>>>> 12edd0b (added lab7)
       if (this.onTaskDrop && taskId && targetIndex !== null) {
         this.onTaskDrop(taskId, this.status, targetIndex);
       }
     });
   }
-<<<<<<< HEAD
-=======
-  
-  
->>>>>>> 12edd0b (added lab7)
+
 }
